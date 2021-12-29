32c6f6c709723e603d0ca62d7ac55cdc35a630b5
Pierre GOUDJO <1331326+pierregoudjo@users.noreply.github.com>
2021-12-29 13:56:36 +0100

    Basic f# console app

---
+++ src/Console/Program.fs
@@ -0,0 +1,22 @@
+﻿
+
+// // For more information see https://aka.ms/fsharp-console-apps
+// printfn "Hello from F#"
+
+let fizzbuzz number = 
+  match number with
+  | x when x%3=0 && x%5=0 -> "FizzBuzz"
+  | x when x%3=0 -> "Fizz"
+  | x when x%5=0 -> "Buzz"
+  | x -> string x
+
+[<EntryPoint>]
+let main args =
+  let number = int args[0]
+  [0 .. number] 
+  |> Seq.map fizzbuzz
+  |> Seq.iter (printfn "%s")
+  0
+
+
+
